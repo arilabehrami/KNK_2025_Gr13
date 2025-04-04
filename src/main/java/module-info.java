de65ec60b059cb7ca88539dd8ca53580284d8ea1
arila22 <arila.behrami@student.uni-pr.edu>
2025-04-04 08:46:30 +0200

    Init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.knk_2025_gr13 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.knk_2025_gr13 to javafx.fxml;
+    exports com.example.knk_2025_gr13;
+}
