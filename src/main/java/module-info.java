967e4537b75a9964b23675d0b14c44f35672e0b8
arila22 <arila.behrami@student.uni-pr.edu>
2025-04-04 09:24:57 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.example.knk_2025_gr13 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.knk_2025_gr13 to javafx.fxml;
     exports com.example.knk_2025_gr13;
+
 }
