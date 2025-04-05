8698e094a9a6b6245369fe10c6f4ec1fe8ee998e
Erisa Sollova <erisasollova@gmail.com>
2025-04-05 19:57:05 +0200

    Shtimi i DTO-ve: Create/Update MenyjaDitore dhe Ushqimet

--- src/main/java/models/Dto/MenyjaDitore/CreateMenyjaDitoreDto.java
+++ src/main/java/models/Dto/MenyjaDitore/CreateMenyjaDitoreDto.java
@@ -1,4 +1,29 @@
 package models.Dto.MenyjaDitore;
 
 public class CreateMenyjaDitoreDto {
+
+
+        private String dita;
+        private int grupiID;
+        private int ushqimiID;
+
+        public CreateMenyjaDitoreDto(String dita, int grupiID, int ushqimiID) {
+            this.dita = dita;
+            this.grupiID = grupiID;
+            this.ushqimiID = ushqimiID;
+        }
+
+        public String getDita() {
+            return dita;
+        }
+
+        public int getGrupiID() {
+            return grupiID;
+        }
+
+        public int getUshqimiID() {
+            return ushqimiID;
+        }
+
+
 }
--- src/main/java/models/Dto/Ushqimet/UpdateUshqimetDto.java
+++ src/main/java/models/Dto/Ushqimet/UpdateUshqimetDto.java
@@ -1,4 +1,35 @@
 package models.Dto.Ushqimet;
 
 public class UpdateUshqimetDto {
+
+
+        private int ushqimiID;
+        private String emriUshqimit;
+        private String kategoria;
+        private String pershkrimi;
+
+        public UpdateUshqimetDto(int ushqimiID, String emriUshqimit, String kategoria, String pershkrimi) {
+            this.ushqimiID = ushqimiID;
+            this.emriUshqimit = emriUshqimit;
+            this.kategoria = kategoria;
+            this.pershkrimi = pershkrimi;
+        }
+
+        public int getUshqimiID() {
+            return ushqimiID;
+        }
+
+        public String getEmriUshqimit() {
+            return emriUshqimit;
+        }
+
+        public String getKategoria() {
+            return kategoria;
+        }
+
+        public String getPershkrimi() {
+            return pershkrimi;
+        }
+
+
 }
