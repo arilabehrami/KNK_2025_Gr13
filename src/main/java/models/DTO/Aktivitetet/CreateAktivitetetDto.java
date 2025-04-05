67129c3411f9f525549d51604f1c48c6648dc17d
arila22 <arila.behrami@student.uni-pr.edu>
2025-04-05 17:24:48 +0200

    Init commit

---
+++ src/main/java/models/DTO/Aktivitetet/CreateAktivitetetDto.java
@@ -0,0 +1,47 @@
+package models.DTO.Aktivitetet;
+
+public class CreateAktivitetetDto {
+    private String emriAktivitetit;
+    private String pershkrimi;
+    private String data;
+    private int grupiId;
+
+    public CreateAktivitetetDto(String emriAktivitetit, String pershkrimi, String data, int grupiId) {
+        this.emriAktivitetit = emriAktivitetit;
+        this.pershkrimi = pershkrimi;
+        this.data = data;
+        this.grupiId = grupiId;
+    }
+
+    public String getEmriAktivitetit() {
+        return emriAktivitetit;
+    }
+
+    public void setEmriAktivitetit(String emriAktivitetit) {
+        this.emriAktivitetit = emriAktivitetit;
+    }
+
+    public String getPershkrimi() {
+        return pershkrimi;
+    }
+
+    public void setPershkrimi(String pershkrimi) {
+        this.pershkrimi = pershkrimi;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getGrupiId() {
+        return grupiId;
+    }
+
+    public void setGrupiId(int grupiId) {
+        this.grupiId = grupiId;
+    }
+}
---
+++ src/main/java/models/DTO/Aktivitetet/UpdateAktivitetetDto.java
@@ -0,0 +1,57 @@
+package models.DTO.Aktivitetet;
+
+public class UpdateAktivitetetDto {
+    private int aktivitetiId;
+    private String emriAktivitetit;
+    private String pershkrimi;
+    private String data;
+    private int grupiId;
+
+    public UpdateAktivitetetDto(int aktivitetiId, String emriAktivitetit, String pershkrimi, String data, int grupiId) {
+        this.aktivitetiId = aktivitetiId;
+        this.emriAktivitetit = emriAktivitetit;
+        this.pershkrimi = pershkrimi;
+        this.data = data;
+        this.grupiId = grupiId;
+    }
+
+    public int getAktivitetiId() {
+        return aktivitetiId;
+    }
+
+    public void setAktivitetiId(int aktivitetiId) {
+        this.aktivitetiId = aktivitetiId;
+    }
+
+    public String getEmriAktivitetit() {
+        return emriAktivitetit;
+    }
+
+    public void setEmriAktivitetit(String emriAktivitetit) {
+        this.emriAktivitetit = emriAktivitetit;
+    }
+
+    public String getPershkrimi() {
+        return pershkrimi;
+    }
+
+    public void setPershkrimi(String pershkrimi) {
+        this.pershkrimi = pershkrimi;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getGrupiId() {
+        return grupiId;
+    }
+
+    public void setGrupiId(int grupiId) {
+        this.grupiId = grupiId;
+    }
+}
