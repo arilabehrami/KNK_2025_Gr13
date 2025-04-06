c26e033d6aa16c272bebae160a385b6a5800f794
arila22 <arila.behrami@student.uni-pr.edu>
2025-04-06 15:18:51 +0200

    Init commit

---
+++ src/main/java/models/Dto/CreatePagesaDto.java
@@ -0,0 +1,52 @@
+package models.dto;
+
+public class CreatePagesaDto {
+    private int pagesaId;
+    private int femijaId;
+    private double shuma;
+    private String data;
+    private String pershkrimi;
+    public CreatePagesaDto(int pagesaId, int femijaId, double shuma, String data, String pershkrimi){
+        this.pagesaId = pagesaId;
+        this.femijaId = femijaId;
+        this.shuma = shuma;
+        this.data = data;
+        this.pershkrimi = pershkrimi;
+    }
+
+    public int getPagesaId(){
+        return pagesaId;
+    }
+
+    public void setFemijaId(int femijaId){
+        this.femijaId = femijaId;
+    }
+
+    public int getFemijaId(){
+        return femijaId;
+    }
+
+    public void setShuma(double shuma){
+        this.shuma = shuma;
+    }
+
+    public double getShuma(){
+        return shuma;
+    }
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+    public void setPershkrimi(String pershkrimi){
+        this.pershkrimi = pershkrimi;
+    }
+
+    public String getPershkrimi(){
+        return pershkrimi;
+    }
+}
