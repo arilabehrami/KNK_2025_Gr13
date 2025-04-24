44fcc66172bfb610644ebedf79b89bf627d947fb
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2025-04-24 20:46:18 +0200

    Create FemijetRepository.java

--- src/main/java/repository/FemijetRepository.java
+++ src/main/java/repository/FemijetRepository.java
@@ -1,4 +1,72 @@
 package repository;
 
-public class FemijetRepository {
+import models.domain.Femijet;
+import models.dto.Femijet.CreateFemijetDto;
+import models.dto.Femijet.UpdateFemijetDto;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class FemijetRepository extends BaseRepository<Femijet, CreateFemijetDto, UpdateFemijetDto> {
+    public FemijetRepository() {
+        super("femijet");
+    }
+
+    @Override
+    public Femijet fromResultSet(ResultSet result) throws SQLException {
+        return Femijet.getInstance(result);
+    }
+
+    @Override
+    public Femijet create(CreateFemijetDto femijetDto) {
+        String query = """
+            INSERT INTO
+            FEMIJET (Emri, Mbiemri, DataLindjes, Gjinia, Adresa, EmriPrindit, KontaktiPrindit)
+            VALUES (?, ? , ?, ?, ?, ?, ?)
+            """;
+        try {
+            PreparedStatement pstm = this.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            pstm.setString(1, femijetDto.getEmri());
+            pstm.setString(2, femijetDto.getMbiemri());
+            pstm.setString(3, femijetDto.getDataLindjes());
+            pstm.setBoolean(4, femijetDto.isGjinia());
+            pstm.setString(5, femijetDto.getAdresa());
+            pstm.setString(6, femijetDto.getEmriPrindit());
+            pstm.setString(7, femijetDto.getKontaktiPrindit());
+            pstm.execute();
+
+            ResultSet res = pstm.getGeneratedKeys();
+            if (res.next()) {
+                int id = res.getInt(1);
+                return this.getByfemijaId(id);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Femijet update(UpdateFemijetDto femijetDto) {
+        String query = """
+                UPDATE FEMIJET
+                SET Emri = ?
+                WHERE FemijaID = ?
+                """;
+        try {
+            PreparedStatement pstm = this.connection.prepareStatement(query);
+            pstm.setString(1, femijetDto.getEmri());
+            pstm.setInt(2, femijetDto.getFemijaID());
+            int updatedRecords = pstm.executeUpdate();
+            if (updatedRecords == 1) {
+                return this.getByfemijaId(femijetDto.getFemijaID());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
+
