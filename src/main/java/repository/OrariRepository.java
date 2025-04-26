22bfc136ec0b0a57a543ce77ddfec22b0de06d15
arila22 <arila.behrami@student.uni-pr.edu>
2025-04-26 21:42:42 +0200

    Create OrariRepository.java

---
+++ src/main/java/repository/OrariRepository.java
@@ -0,0 +1,70 @@
+package repository;
+
+import models.domain.Orari;
+import models.Dto.Orari.CreateOrariDto;
+import models.Dto.Orari.UpdateOrariDto;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class OrariRepository extends BaseRepository<Orari, CreateOrariDto, UpdateOrariDto> {
+
+    public OrariRepository() {
+        super("Orari");
+    }
+
+    @Override
+    Orari fromResultSet(ResultSet res) throws SQLException {
+        return Orari.getInstance(res);  // Nevojitet implementimi i metodës getInstance në klasën Orari
+    }
+
+    @Override
+    public Orari create(CreateOrariDto createDto) {
+        String query = """
+                INSERT INTO Orari (FemijaID, Dita, OraHyrjes, OraDaljes)
+                VALUES (?, ?, ?, ?)
+                """;
+        try {
+            PreparedStatement pstm = this.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            pstm.setInt(1, createDto.getFemijaId());
+            pstm.setString(2, createDto.getDita());
+            pstm.setString(3, createDto.getOraHyrjes());
+            pstm.setString(4, createDto.getOraDaljes());
+            pstm.execute();
+            ResultSet res = pstm.getGeneratedKeys();
+            if (res.next()) {
+                int id = res.getInt(1);
+                return this.getById(id);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Orari update(UpdateOrariDto updateDto) {
+        String query = """
+                UPDATE Orari
+                SET FemijaID = ?, Dita = ?, OraHyrjes = ?, OraDaljes = ?
+                WHERE OrariID = ?
+                """;
+        try {
+            PreparedStatement pstm = this.connection.prepareStatement(query);
+            pstm.setInt(1, updateDto.getFemijaId());
+            pstm.setString(2, updateDto.getDita());
+            pstm.setString(3, updateDto.getOraHyrjes());
+            pstm.setString(4, updateDto.getOraDaljes());
+            pstm.setInt(5, updateDto.getOrariId());
+            int updatedRecords = pstm.executeUpdate();
+            if (updatedRecords == 1) {
+                return this.getById(updateDto.getOrariId());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
