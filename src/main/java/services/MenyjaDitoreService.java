02da7ab658fe6f9e74d659f57102a45a67dc60e5
Erisa Sollova <erisasollova@gmail.com>
2025-04-27 21:00:03 +0200

    Rregullimi i Sintakses tek MenyjaDitoreService,

--- src/main/java/services/MenyjaDitoreService.java
+++ src/main/java/services/MenyjaDitoreService.java
@@ -13,7 +13,6 @@ public class MenyjaDitoreService {
         this.menyjaDitoreRepository = new MenyjaDitoreRepository();
     }
 
-    // Metoda për të marrë një MenyjaDitore nga ID
     public MenyjaDitore getById(int id) throws Exception {
         if (id <= 0) {
             throw new Exception("ID duhet të jetë më i madh se 0.");
@@ -25,9 +24,7 @@ public class MenyjaDitoreService {
         return menyjaDitore;
     }
 
-    // Metoda për krijimin e një MenyjaDitore
     public MenyjaDitore create(CreateMenyjaDitoreDto createDto) throws Exception {
-        // Validimi i të dhënave
         if (createDto.getDita() == null || createDto.getDita().isEmpty()) {
             throw new Exception("Dita nuk mund të jetë bosh.");
         }
@@ -38,7 +35,6 @@ public class MenyjaDitoreService {
             throw new Exception("UshqimiID duhet të jetë më i madh se 0.");
         }
 
-        // Krijimi i MenyjaDitore
         MenyjaDitore menyjaDitore = this.menyjaDitoreRepository.create(createDto);
         if (menyjaDitore == null) {
             throw new Exception("MenyjaDitore nuk u krijua.");
@@ -46,15 +42,12 @@ public class MenyjaDitoreService {
         return menyjaDitore;
     }
 
-    // Metoda për përditësimin e një MenyjaDitore
     public MenyjaDitore update(UpdateMenyjaDitoreDto updateDto) throws Exception {
-        // Verifikim se ekziston MenyjaDitore për përditësim
         MenyjaDitore ekzistues = this.getById(updateDto.getMenuID());
         if (ekzistues == null) {
             throw new Exception("MenyjaDitore për përditësim nuk ekziston.");
         }
 
-        // Përshtat update në repository
         return this.menyjaDitoreRepository.update(updateDto);
     }
 }
