3f61d05354f88d49619bd396528d51dc5d6a730d
lizesyla <155230620+lizesyla@users.noreply.github.com>
2025-04-28 15:04:50 +0200

    create

--- src/main/java/services/PagesaService.java
+++ src/main/java/services/PagesaService.java
@@ -1,10 +1,38 @@
 package services;
 
+import models.domain.Pagesa;
+import models.dto.Pagesa.CreatePagesaDto;
 import repository.PagesaRepository;
 
 public class PagesaService {
     private PagesaRepository pagesaRepository;
     public PagesaService(){
-        
+        this.pagesaRepository = new PagesaRepository();
     }
+    public Pagesa getById(int id) throws Exception{
+        if(id>=0){
+            throw new Exception("Id duhet te jete me e madhe se 0");
+        }
+        Pagesa pagesa = this.pagesaRepository.getById(id);
+        if(pagesa == null){
+            throw new Exception("Aktiviteti me id " + id +"nuk u gjet!");
+        }
+        return pagesa;
+    }
+
+    public Pagesa create(CreatePagesaDto createPagesaDto) throws Exception{
+        if(createPagesaDto.getFemijaId() == null()){
+            throw new Exception("ID-ja e femijes nuk mund te jete bosh");
+        }
+        if(createPagesaDto.getData()==null || createPagesaDto.getData().isEmpty()){
+            throw new Exception("Data e aktivitetit eshte e detyrueshme");
+        }
+        Pagesa pagesa = this.pagesaRepository.create(createPagesaDto);
+        if(pagesa == null){
+            throw new Exception("Pagesa nuk u krye!");
+        }
+        return pagesa;
+    }
+
+    
 }
