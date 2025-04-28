63157fd1c8b9b9b3bebd5168f7e1595b44b75505
lizesyla <155230620+lizesyla@users.noreply.github.com>
2025-04-28 21:18:18 +0200

    financat dto

---
+++ src/main/java/models/dto/Financat/UpdateFinancatDto.java
@@ -0,0 +1,53 @@
+package models.dto.Financat;
+
+public class UpdateFinancatDto {
+    private int financatID;
+    private String date;
+    private float teArdhura;
+    private float shpenzime;
+    private String pershkrimi;
+
+    public UpdateFinancatDto(int financatID, String date, float teArdhura, float shpenzime, String pershkrimi) {
+        this.financatID = financatID;
+        this.date = date;
+        this.teArdhura = teArdhura;
+        this.shpenzime = shpenzime;
+        this.pershkrimi = pershkrimi;
+    }
+
+    public int getFinancatID() {
+        return financatID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getTeArdhura() {
+        return teArdhura;
+    }
+
+    public void setTeArdhura(float teArdhura) {
+        this.teArdhura = teArdhura;
+    }
+
+    public float getShpenzime() {
+        return shpenzime;
+    }
+
+    public void setShpenzime(float shpenzime) {
+        this.shpenzime = shpenzime;
+    }
+
+    public String getPershkrimi() {
+        return pershkrimi;
+    }
+
+    public void setPershkrimi(String pershkrimi) {
+        this.pershkrimi = pershkrimi;
+    }
+}
