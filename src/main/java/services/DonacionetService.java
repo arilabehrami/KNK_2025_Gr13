0806d44eedcd3fcf391a9127d35c138289e71636
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2025-05-03 19:37:58 +0200

    Create DonacionetService.java

---
+++ src/main/java/services/DonacionetService.java
@@ -0,0 +1,55 @@
+package services;
+
+import models.domain.Donacionet;
+import models.dto.Donacionet.CreateDonacionetDto;
+import models.dto.Donacionet.UpdateDonacionetDto;
+import repository.DonacionetRepository;
+
+public class DonacionetService {
+    private DonacionetRepository donacionetRepository;
+
+    public DonacionetService() {
+        this.donacionetRepository = new DonacionetRepository();
+    }
+
+
+    public Donacionet getById(int id) throws Exception {
+        if (id <= 0) {
+            throw new Exception("ID duhet të jetë më i madh se 0.");
+        }
+        Donacionet donacioni = this.donacionetRepository.getById(id);
+        if (donacioni == null) {
+            throw new Exception("Donacioni me ID " + id + " nuk u gjet.");
+        }
+        return donacioni;
+    }
+
+
+    public Donacionet create(CreateDonacionetDto createDonacionet) throws Exception {
+        if (createDonacionet.getEmriOrganizates() == null || createDonacionet.getEmriOrganizates().isEmpty()) {
+            throw new Exception("Emri i organizatës nuk mund të jetë bosh.");
+        }
+        if (createDonacionet.getLlojiDonatori() == null || createDonacionet.getLlojiDonatori().isEmpty()) {
+            throw new Exception("Lloji i donatorit nuk mund të jetë bosh.");
+        }
+        if (createDonacionet.getShuma() == null || createDonacionet.getShuma() <= 0) {
+            throw new Exception("Shuma e donacionit duhet të jetë më e madhe se 0.");
+        }
+
+        Donacionet donacioni = this.donacionetRepository.create(createDonacionet);
+        if (donacioni == null) {
+            throw new Exception("Donacioni nuk u krijua.");
+        }
+        return donacioni;
+    }
+
+
+    public Donacionet update(UpdateDonacionetDto updateDonacionet) throws Exception {
+        Donacionet ekzistues = this.getById(updateDonacionet.getDonacioniID());
+        if (ekzistues == null) {
+            throw new Exception("Donacioni për përditësim nuk ekziston.");
+        }
+
+        return this.donacionetRepository.update(updateDonacionet);
+    }
+}
