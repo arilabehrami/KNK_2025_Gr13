17899d7018f905f4f92713a25ba5cd74549efeb6
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-11 21:02:22 +0200

    Create OrariController.java

---
+++ src/main/java/controllers/OrariController.java
@@ -0,0 +1,25 @@
+package controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class OrariController {
+
+    @FXML
+    private ComboBox<?> femijaComboBox;
+
+    @FXML
+    private TextField ditaField;
+
+    @FXML
+    private TextField oraHyrjesField;
+
+    @FXML
+    private TextField oraDaljesField;
+
+    @FXML
+    private Button shtoOrarinButton;
+
+    @FXML
+    public void initialize() {
+    }
+}
