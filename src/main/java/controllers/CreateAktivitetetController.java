72eb268a5bb7673ae814f395a4c8c5deeaf9ede0
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-15 00:03:36 +0200

    Implementimi i metodës goToOrari për kalimin nga aktivitetet në orar

--- src/main/java/controllers/CreateAktivitetetController.java
+++ src/main/java/controllers/CreateAktivitetetController.java
@@ -1,12 +1,21 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateAktivitetetController {
 
@@ -44,4 +53,13 @@ public class CreateAktivitetetController {
     private void initialize() {
         grupiComboBox.getItems().addAll("Grupi 1", "Grupi 2", "Grupi 3");
     }
+    @FXML
+    private void goToOrari(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Views/OrariView.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
 }
