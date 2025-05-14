e4716f6dd8733cbaa2f6d244ea90d85ef4550a76
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-15 00:04:30 +0200

    Shtimi i një pjese të kodit në Scene Locator për të menaxhuar lokacionet e skenave

--- src/main/java/utils/SceneLocator.java
+++ src/main/java/utils/SceneLocator.java
@@ -3,7 +3,7 @@ package utils;
 public class SceneLocator {
 
     public static final String MAIN_VIEW = "/Views/MainView.fxml";
-//    public static final String AKTIVITETET_VIEW = "/Views/AktivitetetView.fxml";
-//    public static final String ORARI_VIEW = "/Views/OrariView.fxml";
-//    public static final String PRANIA_VIEW = "/Views/PraniaView.fxml";
+    public static final String AKTIVITETET_VIEW = "/Views/AktivitetetView.fxml";
+    public static final String ORARI_VIEW = "/Views/OrariView.fxml";
+    public static final String PRANIA_VIEW = "/Views/PraniaView.fxml";
 }
