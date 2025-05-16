9c82843eae10ffe01d9fa8352f66c50082398c52
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-16 20:57:26 +0200

    Perditsimi i kontrollerit per tabelen Aktivitetet

--- src/main/java/controllers/CreateAktivitetetController.java
+++ src/main/java/controllers/CreateAktivitetetController.java
@@ -1,71 +1,82 @@
 package controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import services.AktivitetetService;
+import models.Dto.Aktivitetet.CreateAktivitetetDto;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class CreateAktivitetetController {
+    @FXML private TextField emriField;
+    @FXML private TextArea pershkrimiArea;
+    @FXML private DatePicker dataPicker;
+    @FXML private TextField grupiField;
+    @FXML private Button languageButton;
+
+    private final AktivitetetService service = new AktivitetetService();
+    private boolean isAlbanian = true;
 
     @FXML
-    private ComboBox<String> emriAktiviteteriComboBox;
-
-    @FXML
-    private TextArea pershkrimiTextArea;
-
-    @FXML
-    private DatePicker dataDatePicker;
-
-    @FXML
-    private ComboBox<String> grupiComboBox;
-
-    @FXML
-    private Button shtoButton;
-
-    @FXML
-    private void shtoAktivitet() {
-        String emriAktiviteteri = emriAktiviteteriComboBox.getValue();
-        String pershkrimi = pershkrimiTextArea.getText();
-        String data = dataDatePicker.getValue().toString();
-        String grupiID = grupiComboBox.getValue();
-
-
-        System.out.println("Emri: " + emriAktiviteteri);
-        System.out.println("Përshkrimi: " + pershkrimi);
-        System.out.println("Data: " + data);
-        System.out.println("Grupi ID: " + grupiID);
-
+    private void onCreate() {
+        try {
+            CreateAktivitetetDto dto = new CreateAktivitetetDto(
+                    emriField.getText(),
+                    pershkrimiArea.getText(),
+                    dataPicker.getValue().toString(),
+                    Integer.parseInt(grupiField.getText())
+            );
+            service.create(dto);
+            showAlert("Sukses!", "Aktiviteti u shtua me sukses.");
+        } catch (Exception e) {
+            showAlert("Gabim", e.getMessage());
+        }
     }
 
     @FXML
-    private void initialize() {
+    private void toggleLanguage() {
+        isAlbanian = !isAlbanian;
+        Locale locale = new Locale(isAlbanian ? "sq" : "en");
+        ResourceBundle bundle = ResourceBundle.getBundle("languages.messages", locale);
 
-        grupiComboBox.getItems().addAll("Grupi 1", "Grupi 2", "Grupi 3");
-        emriAktiviteteriComboBox.getItems().addAll(
-                "Notim",
-                "Lojra per femije",
-                "Kendim"
-        );
-    }
-    @FXML
-    private void goToOrari(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Views/OrariView.fxml"));
-        Parent root = fxmlLoader.load();
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/AktivitetetView.fxml"), bundle);
+            Parent root = loader.load();
+
+            // Merr controller-in e ri dhe kalo gjuhën
+            CreateAktivitetetController controller = loader.getController();
+            controller.setLanguage(isAlbanian);
+
+            Stage stage = (Stage) languageButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            showAlert("Gabim", "Nuk u arrit të ndërrohet gjuha.");
+        }
     }
 
+    public void setLanguage(boolean isAlbanian) {
+        this.isAlbanian = isAlbanian;
+    }
+
+
+    private void updateTexts(ResourceBundle bundle) {
+        languageButton.setText(bundle.getString("button.language"));
+    }
+
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
 }
