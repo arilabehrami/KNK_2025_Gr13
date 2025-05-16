0a706aa164b5c9d9fbd5a76fb5ddcd037e53fdf6
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2025-05-16 21:38:44 +0200

    Create Main4

---
+++ src/main/java/Main/Main4.java
@@ -0,0 +1,24 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main4 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Views/DonacionetView.fxml"));
+        Scene scene = new Scene(root, 600, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Menaxhimi i Donacioneve");
+        primaryStage.setResizable(false); // Optional: Prevent resizing
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
