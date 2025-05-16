7d25ae5c9455346eac9e613e642f4f9812f405e9
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-16 21:01:10 +0200

    Perditsimi i kontrollerit per tabelen Orari

--- src/main/java/controllers/OrariController.java
+++ src/main/java/controllers/OrariController.java
@@ -1,25 +1,46 @@
 package controllers;
+
+import helpers.LanguageContext;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class OrariController {
 
-    @FXML
-    private ComboBox<?> femijaComboBox;
-
-    @FXML
-    private TextField ditaField;
-
-    @FXML
-    private TextField oraHyrjesField;
-
-    @FXML
-    private TextField oraDaljesField;
-
-    @FXML
-    private Button shtoOrarinButton;
+    @FXML private MenuItem switchLanguageItem;
+    @FXML private Menu fileMenu, editMenu, helpMenu, languageMenu;
+    @FXML private TableColumn<?, ?> ditaColumn, oraHyrjesColumn, oraDaljesColumn, femijaIdColumn;
+    @FXML private TableView<?> orariTable;
+    @FXML private Button backButton;
 
     @FXML
     public void initialize() {
+        // Optional: initialize table data or other components
+    }
+
+    @FXML
+    private void onSwitchLanguage() {
+        // Ndërron gjuhën dhe rifreskon skenën
+        Locale newLocale = LanguageContext.currentLocale.getLanguage().equals("en") ? new Locale("sq") : new Locale("en");
+        LanguageContext.currentLocale = newLocale;
+
+        try {
+            ResourceBundle bundle = ResourceBundle.getBundle("languages.messages", newLocale);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/OrariView.fxml"), bundle);
+            Parent root = loader.load();
+
+            Stage stage = (Stage) switchLanguageItem.getParentPopup().getOwnerWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle(bundle.getString("label.title2"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
