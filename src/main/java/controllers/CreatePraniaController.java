f28eb0a5f746ddec5e1b226870cb408dd272e552
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-16 21:02:52 +0200

    Ndryshova kontrollerin per tabelen Prania

--- src/main/java/controllers/CreatePraniaController.java
+++ src/main/java/controllers/CreatePraniaController.java
@@ -1,36 +1,81 @@
 package controllers;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class CreatePraniaController {
 
     @FXML
-    private ComboBox<String> femijaComboBox;
-    @FXML
-    private DatePicker dataPicker;
-    @FXML
-    private ComboBox<String> statusiComboBox;
-    @FXML
-    private Button ruajButton;
-    @FXML
-    private Label statusLabel;
+    private Label lblFemijaId;
 
+    @FXML
+    private Label lblData;
+
+    @FXML
+    private Label lblStatusi;
+
+    @FXML
+    private TableView<?> praniaTable;
+
+    @FXML
+    private TableColumn<?, ?> colPraniaId, colFemijaId, colData, colStatusi;
+
+    @FXML
+    private TextField tfFemijaId;
+
+    @FXML
+    private DatePicker dpData;
+
+    @FXML
+    private ComboBox<?> cbStatusi;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnChangeLanguage;
+
+    private Locale currentLocale = new Locale("sq"); // default language: Albanian
+
+    @FXML
     public void initialize() {
-        femijaComboBox.getItems().addAll("Femija 1", "Femija 2", "Femija 3");
-        statusiComboBox.getItems().addAll("Aktiv", "Jo Aktiv");
+        updateTexts();
 
-        ruajButton.setOnAction(e -> ruajPranine());
+        btnChangeLanguage.setOnAction(this::handleChangeLanguage);
     }
 
-    private void ruajPranine() {
-        String femijaId = femijaComboBox.getValue();
-        String data = dataPicker.getValue().toString();
-        String statusi = statusiComboBox.getValue();
-
-        statusLabel.setText("Femija: " + femijaId + "\nData: " + data + "\nStatusi: " + statusi);
-
+    private void handleChangeLanguage(ActionEvent event) {
+        if (currentLocale.getLanguage().equals("sq")) {
+            currentLocale = new Locale("en");
+        } else {
+            currentLocale = new Locale("sq");
+        }
+        updateTexts();
     }
+
+    private void updateTexts() {
+        ResourceBundle bundle = ResourceBundle.getBundle("languages.messages", currentLocale);
+
+        colPraniaId.setText(bundle.getString("prania.id"));
+        colFemijaId.setText(bundle.getString("femija.id"));
+        colData.setText(bundle.getString("data"));
+        colStatusi.setText(bundle.getString("statusi"));
+
+        lblFemijaId.setText(bundle.getString("femija.id") + ":");
+        lblData.setText(bundle.getString("data") + ":");
+        lblStatusi.setText(bundle.getString("statusi") + ":");
+
+        btnSave.setText(bundle.getString("btn.save"));
+        btnChangeLanguage.setText(bundle.getString("btn.changeLanguage"));
+    }
+
 }
