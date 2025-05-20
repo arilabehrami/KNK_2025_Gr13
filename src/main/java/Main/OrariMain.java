d79c7a1192ab275205bb6ed602f2fb10cba2df4a
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-20 17:34:14 +0200

    Perditsimi i tabeles Orari

--- src/main/java/Main/OrariMain.java
+++ src/main/java/Main/OrariMain.java
@@ -4,7 +4,6 @@ import controllers.OrariController;
 import Database.DBConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import repository.OrariRepository;
@@ -15,27 +14,28 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class OrariMain extends Application {
-
     @Override
     public void start(Stage stage) throws Exception {
-        // 1. Krijo lidhjen me DB
+        // Merr ResourceBundle për gjuhën
+        ResourceBundle bundle = ResourceBundle.getBundle("languages.messages", Locale.ENGLISH);
+
+        // Ngarko FXML me ResourceBundle
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/OrariView.fxml"), bundle);
+        loader.load();
+
+        // Merr controllerin nga FXMLLoader
+        OrariController controller = loader.getController();
+
+        // Krijo lidhjen me DB
         Connection connection = DBConnection.getConnection();
 
-        // 2. Krijo repo dhe service
-        OrariRepository repo = new OrariRepository(connection);
-        OrariService service = new OrariService(repo);
-        ResourceBundle bundle = ResourceBundle.getBundle("languages.messages", Locale.ENGLISH);
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/OrariView.fxml"), bundle);
 
-        Parent root = loader.load();
-        OrariController controller = loader.getController();
-        controller.setService(service);
-        controller.setResources(bundle);
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        // Krijo dhe shfaq scenën
+        stage.setScene(new Scene(loader.getRoot()));
         stage.setTitle(bundle.getString("orari.title.window"));
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
--- src/main/java/repository/OrariRepository.java
+++ src/main/java/repository/OrariRepository.java
@@ -1,84 +1,67 @@
 package repository;
 
 import models.domain.Orari;
+import models.Dto.Orari.CreateOrariDto;
+import models.Dto.Orari.UpdateOrariDto;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
-public class OrariRepository {
+public class OrariRepository extends BaseRepository<Orari, CreateOrariDto, UpdateOrariDto> {
 
-    private Connection connection;
-
-    public OrariRepository(Connection connection) {
-        this.connection = connection;
+    public OrariRepository() {
+        super("Orari", "OrariID");
     }
 
-    public List<Orari> findAll() throws SQLException {
-        List<Orari> list = new ArrayList<>();
-        String sql = "SELECT OrariID, FemijaID, Dita, ora_hyrjes, ora_daljes FROM Orari";
-        try (Statement stmt = connection.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
-            while (rs.next()) {
-                Orari o = new Orari();
-                o.setOrariID(rs.getInt("OrariID"));
-                o.setFemijaID(rs.getInt("FemijaID"));
-                o.setDita(rs.getString("Dita"));
-                o.setOraHyrjes(rs.getTime("ora_hyrjes").toLocalTime());
-                o.setOraDaljes(rs.getTime("ora_daljes").toLocalTime());
-                list.add(o);
-            }
-        }
-        return list;
+    @Override
+    public Orari fromResultSet(ResultSet rs) throws SQLException {
+        return new Orari(
+                rs.getInt("OrariID"),
+                rs.getInt("FemijaID"),
+                rs.getString("Dita"),
+                rs.getTime("ora_hyrjes").toLocalTime(),
+                rs.getTime("ora_daljes").toLocalTime()
+        );
     }
 
-    public void save(Orari o) throws SQLException {
+    @Override
+    public Orari create(CreateOrariDto dto) {
         String sql = "INSERT INTO Orari (FemijaID, Dita, ora_hyrjes, ora_daljes) VALUES (?, ?, ?, ?)";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setInt(1, o.getFemijaID());
-            ps.setString(2, o.getDita());
-            ps.setTime(3, Time.valueOf(o.getOraHyrjes()));
-            ps.setTime(4, Time.valueOf(o.getOraDaljes()));
+        try (PreparedStatement ps = this.connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setInt(1, dto.getFemijaID());
+            ps.setString(2, dto.getDita());
+            ps.setTime(3, Time.valueOf(dto.getOraHyrjesAsLocalTime()));
+            ps.setTime(4, Time.valueOf(dto.getOraDaljesAsLocalTime()));
             ps.executeUpdate();
-        }
-    }
 
-    public Orari findById(int id) throws SQLException {
-        String sql = "SELECT OrariID, FemijaID, Dita, ora_hyrjes, ora_daljes FROM Orari WHERE OrariID = ?";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setInt(1, id);
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    return new Orari(
-                            rs.getInt("OrariID"),
-                            rs.getInt("FemijaID"),
-                            rs.getString("Dita"),
-                            rs.getTime("ora_hyrjes").toLocalTime(),
-                            rs.getTime("ora_daljes").toLocalTime()
-                    );
-                }
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                int id = rs.getInt(1);
+                return getById(id); // ose krijo objektin manualisht
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return null;
     }
 
-    public void update(Orari o) throws SQLException {
-        String sql = "UPDATE Orari SET FemijaID=?, Dita=?, ora_hyrjes=?, ora_daljes=? WHERE OrariID=?";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setInt(1, o.getFemijaID());
-            ps.setString(2, o.getDita());
-            ps.setTime(3, Time.valueOf(o.getOraHyrjes()));
-            ps.setTime(4, Time.valueOf(o.getOraDaljes()));
-            ps.setInt(5, o.getOrariID());
-            ps.executeUpdate();
-        }
-    }
+    @Override
+    public Orari update(UpdateOrariDto dto) {
+        String sql = "UPDATE Orari SET FemijaID = ?, Dita = ?, ora_hyrjes = ?, ora_daljes = ? WHERE OrariID = ?";
+        try (PreparedStatement ps = this.connection.prepareStatement(sql)) {
+            ps.setInt(1, dto.getFemijaID());
+            ps.setString(2, dto.getDita());
+            ps.setTime(3, Time.valueOf(dto.getOraHyrjesAsLocalTime()));
+            ps.setTime(4, Time.valueOf(dto.getOraDaljesAsLocalTime()));
+            ps.setInt(5, dto.getOrariID());
 
-    public void delete(int id) throws SQLException {
-        String sql = "DELETE FROM Orari WHERE OrariID = ?";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setInt(1, id);
-            ps.executeUpdate();
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows > 0) {
+                return getById(dto.getOrariID());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return null;
     }
 }
